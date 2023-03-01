57ff8eb616400657522643743a785008cf2bdba2
Skorp942 <stalker22294_94@mail.ru>
2023-03-01 09:17:26 +0700

    Сдано

--- src/main.java
+++ src/main.java
@@ -2,10 +2,28 @@ import java.util.Random;
 import java.util.Scanner;
 
 class TicTacToe {
-    final char SIGN_X = 'X';
-    final char SIGN_O = 'O';
-    final char SIGN_EMPTY = '.';
-    char[][] table;
+    interface Basic{
+        char getChar();
+    }
+    class XXX implements Basic{
+        public char getChar(){
+            return 'X';
+        }
+    }
+    class OOO implements Basic{
+        public char getChar(){
+            return  '0';
+        }
+    }
+    class Empty implements Basic{
+        public char getChar(){
+            return '*';
+        }
+    }
+    final Basic SIGN_X = new XXX();
+    final Basic SIGN_O = new OOO();
+    final Basic SIGN_EMPTY = new Empty();
+    Basic[][] table;
     Random random;
     Scanner scanner;
 
@@ -16,7 +34,7 @@ class TicTacToe {
     TicTacToe() {
         random = new Random();
         scanner = new Scanner(System.in);
-        table = new char[3][3];
+        table = new Basic[3][3];
     }
 
     void game() {
@@ -53,7 +71,7 @@ class TicTacToe {
     void printTable() {
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++)
-                System.out.print(table[row][col] + " ");
+                System.out.print(table[row][col].getChar() + " ");
             System.out.println();
         }
     }
@@ -81,17 +99,17 @@ class TicTacToe {
         } while (!isCellValid(x, y));
         table[y][x] = SIGN_X;
     }
-    boolean checkWin(char dot) {
+    boolean checkWin(Basic dot) {
         for (int i = 0; i < 3; i++)
-            if ((table[i][0] == dot && table[i][1] == dot &&
-                    table[i][2] == dot) ||
-                    (table[0][i] == dot && table[1][i] == dot &&
-                            table[2][i] == dot))
+            if ((table[i][0].equals(dot) && table[i][1].equals(dot) &&
+                    table[i][2].equals(dot)) ||
+                    (table[0][i].equals(dot) && table[1][i].equals(dot) &&
+                            table[2][i].equals(dot)))
                 return true;
-        return (table[0][0] == dot && table[1][1] == dot &&
-                table[2][2] == dot) ||
-                (table[2][0] == dot && table[1][1] == dot &&
-                        table[0][2] == dot);
+        return (table[0][0].equals(dot) && table[1][1].equals(dot) &&
+                table[2][2].equals(dot)) ||
+                (table[2][0].equals(dot) && table[1][1].equals(dot) &&
+                        table[0][2].equals(dot));
     }
     boolean isTableFull() {
         for (int row = 0; row < 3; row++)
